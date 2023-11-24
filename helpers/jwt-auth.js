62423a91ded7ee6abaf159bd984760c5a08a8a32
nazat47 <nazat.mf@gmail.com>
2023-11-24 22:01:13 +0600

    added doc

--- helpers/jwt-auth.js
+++ helpers/jwt-auth.js
@@ -11,9 +11,11 @@ function authjwt() {
       { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
       `/api/v1/auth/login`,
       `/api/v1/auth/register`,
       `/`,
+      `/api-doc`
     ],
   });
 }
